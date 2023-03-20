3b536a2332c254edde5146a5bb4b81f464bb7c32
Olga <data19101988@gmail.com>
2023-03-21 00:30:57 +0300

    telebot working

---
+++ app.py
@@ -0,0 +1,29 @@
+import telebot
+
+TOKEN = '6091943863:AAFubE3amsd38QZMJaGSKzVxrg2CBRx1GU4'
+
+keys = {
+    'биткоин':'BTC',
+    'эфириум':'ETH',
+    'доллар': 'USD'
+}
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу с валютой нужно ввести команду в чате:\n<имя валюты> \
+<в какую валюту перевести> \
+<какое количество нужно переввести>\
+Увидеть список всех доступных валют: <curr>'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['curr'])
+def available_currencies(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+
+    for k in keys:
+        text = '\n'.join((text, k))
+    bot.reply_to(message, text)
+
+bot.polling()
